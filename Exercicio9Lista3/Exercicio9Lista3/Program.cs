6b9ae39cc6e0277ed85d2e2ce662efb2526c38c8
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-08-17 01:58:38 -0300

    Add files via upload

---
+++ Exercicio9Lista3/Exercicio9Lista3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio9Lista3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inicializa os dois primeiros números da série de Fibonacci
+            int num1 = 1;
+            int num2 = 1;
+
+            // Exibe os dois primeiros números
+            Console.WriteLine($"Os trinta primeiros valores da série de Fibonacci:");
+
+            // Loop para calcular e exibir os trinta primeiros valores
+            for (int i = 0; i < 30; i++)
+            {
+                // Exibe o número atual da série
+                Console.WriteLine(num1);
+
+                // Calcula o próximo número da série somando os dois últimos números
+                int proximoNumero = num1 + num2;
+
+                // Atualiza os dois últimos números para os próximos cálculos
+                num1 = num2;
+                num2 = proximoNumero;
+            }
+        }
+    }
+}
